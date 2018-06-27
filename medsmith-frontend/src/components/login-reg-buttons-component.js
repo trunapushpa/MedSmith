bde4349ca963d6001bc47c3218f74dd21245b389
Trunapushpa Surkar <trunapushpa@gmail.com>
2018-06-27 23:33:43 +0530

    Styling Updated

--- medsmith-frontend/src/components/login-reg-buttons-component.js
+++ medsmith-frontend/src/components/login-reg-buttons-component.js
@@ -5,21 +5,21 @@ import '../css/App.css'
 class LoginRegButtonsComponent extends Component {
     render() {
         return (
-            <div className={"container App col-lg-7"}>
-                <div className={"container"}>
-                    <div className="mycard" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-                        <div className="card bg-light border-0 mb-3" style={{marginRight: '40px',marginLeft: '40px', marginTop: '40px', marginBottom: '40px'}}>
+            <div className={"container App"}>
+                <div className="mycard" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+                    <div className="card bg-light border-0 mb-3"
+                        style={{marginLeft: '20px', marginRight: '20px', marginTop: '40px', marginBottom: '40px'}}>
                         <h3>Already have an account</h3><br/>
                         <Link to="/login">
                             <button className={"btn btn-primary btn-lg"}>Login</button>
                         </Link>
-                        </div>
-                        <div className="card bg-light border-0 mb-3" style={{marginRight: '40px',marginLeft: '40px', marginTop: '40px', marginBottom: '40px'}}>
+                    </div>
+                    <div className="card bg-light border-0 mb-3"
+                        style={{marginLeft: '20px', marginRight: '20px', marginTop: '40px', marginBottom: '40px'}}>
                         <h3>Do not have an account</h3><br/>
                         <Link to="/register">
                             <button className={"btn btn-primary btn-lg"}>Register</button>
                         </Link>
-                        </div>
                     </div>
                 </div>
             </div>
--- medsmith-frontend/src/components/reg-buttons-component.js
+++ medsmith-frontend/src/components/reg-buttons-component.js
@@ -5,20 +5,29 @@ import '../css/App.css'
 class RegButtonsComponent extends Component {
     render() {
         return (
-            <div className={"container App col-lg-7"}>
-                <div className={"jumbotron"}>
-                    <Link to="/patient_register">
-                        <button className={"btn btn-primary btn-lg"}>Patient Registration</button>
-                    </Link>
-                    <br/><br/>
-                    <Link to="/doctor_register">
-                        <button className={"btn btn-primary btn-lg"}>Doctor Registration</button>
-                    </Link>
-                    <br/><br/>
-                    <Link to="/hospital_register">
-                        <button className={"btn btn-primary btn-lg"}>Hospital Registration</button>
-                    </Link>
-                    <br/><br/>
+            <div className="container">
+                <div className="mycard" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+                    <div className="card bg-light border-0 mb-3"
+                         style={{marginLeft:'25px', marginRight:'25px', marginTop: '30px', marginBottom: '40px'}}>
+                        <h4>Register as a Patient</h4><br/>
+                        <Link to="/patient_register">
+                            <button className={"btn btn-primary btn-lg"}>Patient Registration</button>
+                        </Link>
+                    </div>
+                    <div className="card bg-light border-0 mb-3"
+                         style={{marginLeft:'25px', marginRight:'25px', marginTop: '30px', marginBottom: '40px'}}>
+                        <h4>Register as a Doctor</h4><br/>
+                        <Link to="/doctor_register">
+                            <button className={"btn btn-primary btn-lg"}>Doctor Registration</button>
+                        </Link>
+                    </div>
+                    <div className="card bg-light border-0 mb-3"
+                         style={{marginLeft:'25px', marginRight:'25px', marginTop: '30px', marginBottom: '40px'}}>
+                        <h4>Register as a Hospital</h4><br/>
+                        <Link to="/hospital_register">
+                            <button className={"btn btn-primary btn-lg"}>Hospital Registration</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         );
